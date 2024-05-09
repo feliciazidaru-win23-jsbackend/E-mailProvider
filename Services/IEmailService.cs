bd64eaac1841674250b72e943e7c5fc916ec0a6e
felyc <fely.coman@gmail.com>
2024-05-09 08:39:30 +0200

    EmailSender configured succesfully

---
+++ Services/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Messaging.ServiceBus;
+using E_mailProvider.Models;
+
+namespace E_mailProvider.Services;
+
+    public interface IEmailService
+    {
+        bool SendEmail(EmailRequest emailRequest);
+        EmailRequest UnpackEmailRequest(ServiceBusReceivedMessage message);
+    }
